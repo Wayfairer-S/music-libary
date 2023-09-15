994757f0148f3f973fa0517c5b5bc50d9fec58f2
Peter Skerritt <peterskerritthintsbrook@gmail.com>
2023-09-15 17:22:13 +0100

    Added controller function to create and artist and add to the DB.

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,6 +1,18 @@
+const db = require("../db/index.js");
+
 const createArtist = async (req, res) => {
   const { name, genre } = req.body;
-  res.sendStatus(201);
+  try {
+    const {
+      rows: [artist],
+    } = await db.query(
+      `INSERT INTO Artists (name, genre) VALUES ($1, $2) RETURNING *`,
+      [name, genre]
+    );
+    res.status(201).json(artist);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
 };
 
 module.exports = { createArtist };
